739aebb2cf78c2d17f0b311e1a6e110d22164905
Kanthi Kumar Dondapati <84696870+kanthikumarkk11@users.noreply.github.com>
2021-06-04 14:52:44 +0530

    Update wallis.c

--- wallis.c
+++ wallis.c
@@ -2,7 +2,14 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <math.h>
-
+float wallis_pi(int count){
+    float pi = 1.0;
+    for (int i = 1; i < count; i++){
+        float num = 4.0 * i * i;
+        pi *= num / (num - 1);
+    }
+    return pi * 2;
+}
 float wallis_pi(int);
 
 int main(void) {
